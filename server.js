99cfe5fb4126b3003519db09fc0f87d1957966f4
IshikaAnand7 <anandishika07@gmail.com>
2025-04-13 21:10:22 +1000

    first commit

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
+
 const express = require('express');
-const path = require('path');
-
 const app = express();
-const PORT = 3000;
+const path = require('path');
+const port = process.env.PORT || 3001;
 
-
-app.use(express.static('public'));
+// Serve static files from the 'public' folder using the absolute path
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html')); // Ensure the correct file path
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
